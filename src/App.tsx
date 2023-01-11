1f43af1854d01d11401d1d203b378357aca15703
kenizaya <harveylstark@gmail.com>
2023-01-11 17:28:17 +0530

    Setup Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Actors from './pages/Actors'
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+import MovieInfo from './pages/MovieInfo'
+import Movies from './pages/Movies'
+import Profile from './pages/Profile'
 
 const App = () => {
-  return <div className='text-3xl font-bold'>App</div>
+  return (
+    <>
+      <Navbar />
+      <main className='text-3xl font-bold'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='movies' element={<Movies />} />
+          <Route path='movies/:movieId' element={<MovieInfo />} />
+          <Route path='actors' element={<Actors />} />
+          <Route path='profile/:profileId' element={<Profile />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+      </main>
+    </>
+  )
 }
 
 export default App
