4c2bf4eb447fd09b50c21f9285b67d9c8242af68
kenizaya <harveylstark@gmail.com>
2023-03-01 17:27:10 +0530

    BREAKING CHANGES - Clean UP

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Actors from './pages/Actors'
-import ErrorPage from './pages/ErrorPage'
-import Home from './pages/Home'
-import MovieInfo from './pages/MovieInfo'
-import Movies from './pages/Movies'
-import Profile from './pages/Profile'
 
 const App = () => {
-  return (
-    <>
-      <div className='grid'>
-        <Sidebar />
-      </div>
-    </>
-  )
+  return <Navbar />
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
